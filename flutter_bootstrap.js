b62f271fc53d0ec799c698494a57e73344c871fc
EvangelionsFelix8 <felixyehdeya@gmail.com>
2025-12-09 22:06:13 +0700

    update kembalian

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"035316565ad77281a75305515e4682e6c4c6f7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2855510926"
+    serviceWorkerVersion: "142141714"
   }
 });
